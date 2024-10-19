0b3b9e89075d362eeb2e3d3ffeccbc69d5acfa8c
notjameskang <jamesk02@vt.edu>
2024-10-19 09:10:51 -0400

    React.JS integration

---
+++ DineSmart/src/App.jsx
@@ -0,0 +1,12 @@
+import Home from './assets/Components/HomePage/home.jsx'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Home/>
+      
+    </div>
+  )
+}
+
+export default App
